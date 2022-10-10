dbcea424375034c05e2916579c2f0598364ae140
nchola <108863217+nchola@users.noreply.github.com>
2022-10-10 15:03:50 +0700

    hi
    hi

---
+++ app/src/main/java/com/muhammadnanda/belajarexplicitintent/MainActivity.java
@@ -0,0 +1,14 @@
+package com.muhammadnanda.belajarexplicitintent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
